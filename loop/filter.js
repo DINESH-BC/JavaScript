07500df9ebfc4d65743758010f61e7889ba1719e
Dinesh BC <bcdinesh91@gmail.com>
2025-04-29 13:32:16 +0545

    filter method using  with books

--- loop/filter.js
+++ loop/filter.js
@@ -20,3 +20,35 @@ const myNum2 =  myNums.filter((num)=>{
 
 
 console.log(myNum2);
+
+
+
+// The filter with like real world project 
+
+
+const books = [
+    { title: "To Kill a Mockingbird", author: "Harper Lee", genre: "Fiction", year: 1960 },
+
+    { title: "1984", author: "George Orwell", genre: "Dystopian", year: 1949 },
+
+    { title: "The Great Gatsby", author: "F. Scott Fitzgerald", genre: "Classic", year: 1925 },
+
+    { title: "Pride and Prejudice", author: "Jane Austen", genre: "Romance", year: 1813 },
+
+    { title: "The Catcher in the Rye", author: "J.D. Salinger", genre: "Fiction", year: 1951 },
+
+    { title: "The Hobbit", author: "J.R.R. Tolkien", genre: "Fantasy", year: 1937 },
+
+    { title: "Moby Dick", author: "Herman Melville", genre: "Adventure", year: 1851 },
+
+    { title: "War and Peace", author: "Leo Tolstoy", genre: "Historical", year: 1869 },
+
+    { title: "The Alchemist", author: "Paulo Coelho", genre: "Philosophical", year: 1988 },
+
+    { title: "Brave New World", author: "Aldous Huxley", genre: "Science Fiction", year: 1932 }
+  ];
+  
+
+  const myBooks = books.filter((book) => (book.genre === "Philosophical" && book.year >= 1988))
+
+  console.log(myBooks)
