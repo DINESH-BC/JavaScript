b47ade132f06428b5e3f7075d0f39504e8ce8a79
Dinesh BC <bcdinesh91@gmail.com>
2025-04-29 13:02:16 +0545

    simple example of filter

---
+++ loop/filter.js
@@ -0,0 +1,13 @@
+
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newNumber = myNums.filter( (number) =>(number > 4 && number <7))
+
+console.log(newNumber);
+
+
+
+console.log(myNums.filter( (number) =>(number > 6)));
+
+
